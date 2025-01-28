2d573969e57ff06794b306e3072eb24bb7db7ec9
Mihail Rulinskiy <roulinski@hotmail.com>
2025-01-28 15:47:55 +0200

    Update backend logic and dependencies for login system

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cardAccountRouter = require('./routes/card_account');
 const transactionRoutes = require('./routes/transaction');
 const accountRouter = require('./routes/account');
 const customerRouter = require('./routes/customer');
+const loginRouter = require('./routes/login');
 
 db.getConnection((err, connection) => {
   if (err) {
@@ -20,6 +21,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var cardRouter = require('./routes/card');
+const jwt = require('jsonwebtoken');
 
 var app = express();
 
@@ -30,10 +32,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use(authenticateToken);
 app.use('/card', cardRouter);
 app.use('/card_account', cardAccountRouter);
 app.use('/transaction', transactionRoutes);
 app.use('/account', accountRouter);
 app.use('/customer', customerRouter);
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  console.log("token = "+token);
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.MY_TOKEN, function(err, user) {
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+
 
 module.exports = app;
