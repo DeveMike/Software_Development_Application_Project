ed3e9986ff725a1364f12074ef7b8589fa462300
andyffl <t2koan07@students.oamk.fi>
2025-02-14 12:27:19 +0200

    Inactivity timer, transaction fetching fix, multilingual support & bug fixes

--- bank-automat/transaction.h
+++ bank-automat/transaction.h
@@ -9,6 +9,7 @@
 #include <QJsonObject>
 #include <QCloseEvent>
 #include <QTableWidget>
+#include <QTimer>
 
 namespace Ui {
 class TransactionWindow;
@@ -22,16 +23,23 @@ public:
     explicit TransactionWindow(QWidget *parent = nullptr);
     ~TransactionWindow();
     void setLanguage(const QString &newLanguage);
-    void fetchTransactions(const QString &idcard, const QByteArray &token);
+    void fetchTransactions(const QString &idcard, const QString &cardMode, const QByteArray &token);
+    void fetchTransactionsByAccount(int accountId, const QByteArray &token);
+    void checkInactivity();
 
 private slots:
     void onTransactionDataReceived(QNetworkReply *reply);
+    void onCardAccountReceived(QNetworkReply *reply, const QString &idcard, const QString &cardMode, const QByteArray &token);
     void on_btnNext_clicked();
     void on_btnPrev_clicked();
     void on_btnBack_clicked();
+    void on_btnBack_2_clicked();
 
 protected:
     void closeEvent(QCloseEvent *event) override;
+    void showEvent(QShowEvent *event) override;
+    void hideEvent(QHideEvent *event) override;
+    bool eventFilter(QObject *obj, QEvent *event) override;
 
 private:
     Ui::TransactionWindow *ui;
@@ -40,7 +48,7 @@ private:
     QList<QJsonObject> transactionsList;
     int currentPage;
     const int itemsPerPage = 10;
-
+    QTimer *inactivityTimer;
     void updateLanguage();
     void updateTransactionList();
 };
